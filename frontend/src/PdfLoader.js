a594733ff5d00e0352588683db5aa3314961d8f7
RCristyan <rcristyan9@gmail.com>
2021-11-10 09:45:09 -0300

    Add frontend initial structure.

---
+++ frontend/src/PdfLoader.js
@@ -0,0 +1,9 @@
+function PdfLoader(){
+    return(
+        <div className="PdfLoader">
+            <h1>Pdf loader</h1>
+        </div>
+    )
+}
+
+export default PdfLoader;
---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PdfLoader from './PdfLoader';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PdfLoader />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
